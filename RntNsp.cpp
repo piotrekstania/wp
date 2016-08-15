63d6803bd6380ba00d0caeb4df63e4886c57eadb
piotrekstania <piotrekstania@gmail.com>
2016-08-15 07:55:49 +0200

    +

--- RntNsp.cpp
+++ RntNsp.cpp
@@ -1,107 +1,76 @@
-#include <cstdlib>
+#include <chrono>
 #include <cstring>
 #include <wiringPi.h>
 #include "RntNsp.h"
-#include <iostream>
-#include <chrono>
 
-using namespace std;
 using namespace std::chrono;
 
-uint8_t crc8(uint8_t *buffer, uint8_t size);
+RntNsp::RntNsp() {
+	clearBuffer();
+}
 
-// Ramka +/- 100 us:
-// 	- start: 4000 us (3900..4100)
-// 	- bit 0:  300 us (200..400)
-// 	- bit 1:  700 us (600..800)
-// 	- stop:	 2000 us (1900..2100)
 
-#define START_MIN		4000
-#define START_MAX		6000
+void RntNsp::clearBuffer() {
+	memset(buff, 0, NSP_BUFF_SIZE);
+	bit = 0;
+	byte = 0;
+	start = false;
+	stop = false;
+}
 
-#define BIT0_MIN		250
-#define BIT0_MAX		649
+void RntNsp::checkBuffer() {
+	frameCounter++;
 
-#define BIT1_MIN		650
-#define BIT1_MAX		1100
+	if(byte != 0) errFrame++;
+	else if((buff[0] != '$') || (buff[NSP_BUFF_SIZE-1] != '#')) errHeader++;
+	else if(crc8(buff, NSP_BUFF_SIZE-2) != buff[NSP_BUFF_SIZE-2]) errCrc++;
+	else {
+		//dekodowanie ramki
+	}
 
-#define STOP_MIN		1900
-#define STOP_MAX		2100
-
+	clearBuffer();
+}
 
 void RntNsp::tick(int state) {
+
 	uint64_t tick = duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
 	uint64_t diff = (tick - lastTick);
 	lastTick = tick;
 
-	if(state == 0) return;
+	if((state == 0) || (stop == true)) return;
 
-	if((diff >= START_MIN) && (diff <= START_MAX)) {
-		if(by != 0) errFrame++;
-		bi = 0;
-		by = 0;
-		memset(buf, 0, sizeof(buf));
-		res = true;
-		return;
-	} else if((diff >= BIT0_MIN) && (diff <= BIT0_MAX)) {
-		if(res==true) bi++;
-	} else if((diff >= BIT1_MIN) && (diff <= BIT1_MAX)) {
-		if(res==true) {buf[by] |= (1<<bi); bi++;};
-	} else if(res==true) {
-		errTime++;
-		res = false;
-	}
-
-
-	if(bi == 8) {
-		bi = 0;
-		by++;
-	}
-
-	if(by == 10) {
-
-		frame++;
-		system ("clear");
-		cout<<"Frame: "<<frame<<", ";
-
-		bool ok = true;
-
-		if((buf[0] != '$') || (buf[9] != '#')) {
-			errHeader++;
-			ok = false;
-		} else if(buf[8] != crc8(buf, 8)) {
-			errCrc++;
-			ok = false;
+	if((diff >= NSP_START_MIN) && (diff <= NSP_START_MAX)) {
+		if((bit != 0) || (byte != 0)) {
+			errFrame++;
+			clearBuffer();
 		}
-
-
-		int16_t temp = buf[4];
-		temp <<= 8;
-		temp += buf[5];
-		temp /= 10;
-
-		uint16_t rh = buf[6];
-		rh <<= 8;
-		rh += buf[7];
-		rh /= 10;
-
-		bi = 0;
-		by = 0;
-		res = false;
-		memset(buf, 0, sizeof(buf));
-
-		uint32_t total = (errTime+errFrame+errHeader+errCrc);
-		float proc = (total * 10000)/frame;
-		proc /= 100;
-		cout<<"Errors: "<<total<<" ("<<proc<<" %)"<<" (Time: "<<errTime<<" Frame: "<<errFrame<<" Header: "<<errHeader<<" Crc: "<<errCrc<<")"<<endl;
-                if(ok) cout<<"T: "<<(temp/10)<<"."<<(temp%10)<<" °C"<<endl;
-                if(ok) cout<<"H: "<<(rh/10)<<"."<<(rh%10)<<" %"<<endl;
+		start = true;
+		return;
 	}
 
+	if(start == true) {
+		if((diff >= NSP_STOP_MIN) && (diff <= NSP_STOP_MAX)) {
+			stop = true;
+			checkBuffer();
+		} else if((diff >= NSP_BIT0_MIN) && (diff <= NSP_BIT0_MAX)) {
+			buff[byte] &= ~(1<<bit++);
+		} else if((diff >= NSP_BIT1_MIN) && (diff <= NSP_BIT1_MAX)) {
+			buff[byte] |= (1<<bit++);
+		} else {
+			errTime++;
+			clearBuffer();
+		}
+	}
+
+	if(bit == 8) {
+		bit = 0;
+		byte++;
+		if(byte == NSP_BUFF_SIZE) byte = 0;
+	}
 }
 
 
-uint8_t crc8(uint8_t *buffer, uint8_t size) {
+uint8_t RntNsp::crc8(uint8_t *buffer, uint8_t size) {
 
 	uint8_t	 crc = 0;
 	uint8_t loop_count;
@@ -109,27 +78,17 @@ uint8_t crc8(uint8_t *buffer, uint8_t size) {
 	uint8_t  data;
 	uint8_t  feedback_bit;
 
-	for (loop_count = 0; loop_count != size; loop_count++)
-	{
+	for(loop_count = 0; loop_count != size; loop_count++) {
 		data = buffer[loop_count];
-
 		bit_counter = 8;
 		do {
 			feedback_bit = (crc ^ data) & 0x01;
-
-			if ( feedback_bit == 0x01 ) {
-				crc = crc ^ 0x18;	             //0X18 = X^8+X^5+X^4+X^0
-			}
+			if(feedback_bit == 0x01) crc = crc ^ 0x18;	//0X18 = X^8+X^5+X^4+X^0
 			crc = (crc >> 1) & 0x7F;
-			if ( feedback_bit == 0x01 ) {
-				crc = crc | 0x80;
-			}
-
+			if(feedback_bit == 0x01) crc = crc | 0x80;
 			data = data >> 1;
 			bit_counter--;
-
 		} while (bit_counter > 0);
 	}
-
 	return crc;
 }
--- RntNsp.h
+++ RntNsp.h
@@ -3,27 +3,21 @@
 
 #include <cstdint>
 
-//  errory:
-//  - niewlasciwa dlugosc impulsu
-//  - niewlasciwa dlugosc ramki
-//  - niewlasciwe znaki poczatku i konca ramki
-//  - błąd sumy kontrolnej
-//
-//  wspolne:
-//  - maksymalny czas od ostanio dobrze odebranwgo parametru
-//
-// osobne:
-// - parametru
-// - timestamp parametru
-// - dopuszczalny zakres
-//
-// funkcje:
-// - tick
-// - getParam
-// - isValid
-// - setRange
-// - getError
-// - getLastTime
+//wielkosc bufora musi byc rowna dlugosci ramki
+#define NSP_BUFF_SIZE		20
+
+#define NSP_START_MIN		4000
+#define NSP_START_MAX		6000
+
+#define NSP_STOP_MIN		1900
+#define NSP_STOP_MAX		2100
+
+#define NSP_BIT0_MIN		250
+#define NSP_BIT0_MAX		649
+
+#define NSP_BIT1_MIN		650
+#define NSP_BIT1_MAX		1100
+
 
 class RntNsp {
 
@@ -36,37 +30,48 @@ class RntNsp {
 			uint64_t timestamp;
 		} Param;
 
-		uint64_t lastTick;
-
-		Param pressure;
-		Param temperature;
+		Param press;
+		Param temp;
 		Param hppl;
 		Param rh;
 		Param ah;
 
-		uint64_t pressTs;
-		uint64_t tempTs;
-		uint64_t hpplTs;
-		uint64_t rhTs;
-		uint64_t ahTs;
+		uint64_t lastTick;
+		uint64_t lastFrame;
 
-		uint32_t bi;
-		uint32_t by;
-		uint8_t buf[10];
+		uint32_t frameCounter;
+
+		uint8_t bit;
+		uint8_t byte;
+
+		uint8_t buff[NSP_BUFF_SIZE];
+
 		uint32_t errTime;
 		uint32_t errFrame;
 		uint32_t errHeader;
 		uint32_t errCrc;
-		uint32_t frame;
 
-		bool res;
+		bool start;
+		bool stop;
+
+		uint8_t crc8(uint8_t *buffer, uint8_t size);
+		void clearBuffer();
+		void checkBuffer();
+
 	public:
-		enum Edge  {EdgeFall, EdgeRise};
-		enum Param {ParamTemp, ParamRH, ParamAH, ParamPress, ParamHPPL};
+
+		enum Parameter {ParamTemp, ParamRH, ParamAH, ParamPress, ParamHPPL};
+		enum Error {ErrTime, ErrFrame, ErrHeader, ErrCrc};
+
+		RntNsp();
 
 		void tick(int state);
-		bool isValid(Param p);
-		int32_t getParam(Param p);
+		void setRange(Parameter p, int16_t min, int16_t max);
+		bool isValid(Parameter p);
+		int16_t getParam(Parameter p);
+		uint32_t getError(Error e);
+		uint64_t getLastFrameTime();
+		uint32_t getFrameCounter();
 
 };
 
